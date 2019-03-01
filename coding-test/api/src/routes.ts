e533f11f5d585bd5a023c9c2df5f3d63923320ad
yiyi1026 <vivian.li.uibe@gmail.com>
2019-02-28 23:33:47 -0800

    update

--- coding-test/api/src/routes.ts
+++ coding-test/api/src/routes.ts
@@ -1,71 +1,97 @@
-import * as express from 'express';
-export const routes = express.Router();
-const fs = require('fs');
+import * as express from 'express'
+export const routes = express.Router()
+const fs = require('fs')
 
 routes.get('/api/dashboard', (req, res) => {
   fs.readFile('src/data/user_database.csv', function (err: any, data: any) {
-    calcStat(csvJSON(data.toString()));
-    res.send({hello: 'Dashboard'});
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(calcStat(csvJSON(data.toString()))))
+    res.end()
   })
 })
 
-// TODO change the callback to get single user data
-// TODO api/users/all should lead to api/users too
 routes.get('/api/user/:id', (req, res) => {
   fs.readFile('src/data/user_database.csv', function (err: any, data: any) {
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(findByID(csvJSON(data.toString()), req.params.id)));
-    res.end();
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(findByID(csvJSON(data.toString()), req.params.id)))
+    res.end()
   })
 })
 
 routes.get('/api/users', (req, res) => {
   fs.readFile('src/data/user_database.csv', function (err: any, data: any) {
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify(csvJSON(data.toString())));
-    res.end();
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify(csvJSON(data.toString())))
+    res.end()
   })
 })
 
-interface User{
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  age: number;
+interface User {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  age: number
 }
 
-function csvJSON(csv : any){
-  let lines=csv.split("\r\n");
-  let result: User[] = [];
-  let headers=lines[0].split(",");
-  for(let i=1;i<lines.length;i++){
-      let obj: any = {};
-      let currentline=lines[i].split(",");
-      for(let j=0;j<headers.length;j++){
-          obj[headers[j]] = currentline[j];
-      }
-      result.push(obj);
+function csvJSON(csv: any) {
+  let lines = csv.split("\r\n")
+  let result: User[] = []
+  let headers = lines[0].split(",")
+  for (let i = 1; i < lines.length; i++) {
+    let obj: any = {}
+    if(lines[i].trim() == ''){
+      continue
+    }
+    let currentline = lines[i].split(",");
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j]
+    }
+    result.push(obj)
   }
-  return result; //JSON
+  return result
 }
 
-function findByID(usersJson: User[], id: number){
-  return usersJson.find(item => item.id == id);
+function findByID(usersJson: User[], id: number) {
+  return usersJson.find(item => item.id == id)
 }
 
-function calcStat(usersJson: User[]){
-  let num : number = usersJson.length;
-  let totalAge : number = 0;
-  let allAges : number[];
-  let firstNameCount : {[id:string] : number};
-  console.log(usersJson)
-  for(let i=0; i<num; i++){
+function calcStat(usersJson: User[]) {
+  let num: number = usersJson.length
+  let totalAge: number = 0
+  let allAges: number[] = []
+  let medianAge: number = 0
+  let firstNameCount: { [id: string]: number } = {}
+
+  for (let i = 0; i < num; i++) {
     let user = usersJson[i]
-    totalAge += user.age;
-    allAges.push(user.age)
-    // firstNameCount
-
+    totalAge += Number(user.age)
+    allAges.push(Number(user.age))
+    if (firstNameCount[user.first_name]) {
+      firstNameCount[user.first_name] = firstNameCount[user.first_name] + 1
+    } else {
+      firstNameCount[user.first_name] = 1
+    }
   }
-  console.log(totalAge)
+
+  allAges.sort()
+  if (num % 2 === 0) {
+    medianAge = (allAges[num / 2 - 1] + allAges[num / 2]) / 2
+  } else {
+    medianAge = allAges[(num - 1) / 2]
+  }
+
+  var firstNameCountArr = Object.keys(firstNameCount).map(function (key) {
+    return [key, firstNameCount[key]]
+  })
+  firstNameCountArr.sort(function (first: any, second: any) {
+    return second[1] - first[1]
+  })
+  
+  return {
+    number: num,
+    averageAge: (totalAge / num).toFixed(1),
+    medianAge: medianAge,
+    topFiveName: firstNameCountArr.slice(0, 5)
+  }
 }
--- coding-test/api/src/server.ts
+++ coding-test/api/src/server.ts
@@ -1,18 +1,17 @@
-import express from "express";
-import {routes} from "./routes";
+import express from "express"
+import { routes } from "./routes"
 
 const path = require("path")
 const app = express()
 const port: Number = 3001
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  next()
+})
 
-app.set("views", path.join(__dirname, "views"))
-app.use(express.json());
-app.use('/', routes);
+app.use(express.json())
+app.use('/', routes)
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
