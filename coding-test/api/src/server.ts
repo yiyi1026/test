e0220ce76bd4af6b900b4e4a8c8a9c7ee3812a1f
yiyi1026 <vivian.li.uibe@gmail.com>
2019-02-28 20:56:08 -0800

    add api

--- coding-test/api/src/server.ts
+++ coding-test/api/src/server.ts
@@ -2,4 +2,32 @@ import express from "express"
 
 const app = express()
 const port: Number = 3001
+var fs = require('fs');
+
+app.get('/api/getUsers', (req, res) => {
+  fs.readFile('src/data/user_database.csv', function (err: any, data: any) {
+    res.setHeader('Content-Type', 'application/json');
+    res.write(csvJSON(data.toString()));
+    res.end();
+  })
+})
 app.listen(port, () => console.log(`app listening on port ${port}`))
+
+interface IObj{
+  [key: string]: string;
+}
+
+function csvJSON(csv : any){
+  var lines=csv.split("\r\n");
+  var result = [];
+  var headers=lines[0].split(",");
+  for(var i=1;i<lines.length;i++){
+      var obj: IObj = {};
+      var currentline=lines[i].split(",");
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+      result.push(obj);
+  }
+  return JSON.stringify(result); //JSON
+}
